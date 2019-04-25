530095178906b4c5675ba1b4b1ee090569e5afef
mediumwd <medium4uk@gmail.com>
2019-04-25 15:47:35 +0300

    upload commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+    $(".service__accordion__items__item").click(function(){
+        if($(this).children().hasClass("collapsed")){
+        $(".service__accordion__items").find("span").addClass("arrow-down").removeClass("arrow-up");
+        $(this).find("span").addClass("arrow-up").removeClass("arrow-down");
+        }
+        else {
+            $(".service__accordion__items").find("span").addClass("arrow-down").removeClass("arrow-up");
+        }
+    });
+    $('#Carousel').carousel({
+        interval: 5000
+    })
+});
