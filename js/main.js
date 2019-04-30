807e78eadaa3ddbb436a6bcbd59afb3af7dedc5e
mediumwd <medium4uk@gmail.com>
2019-04-30 18:24:32 +0300

    beta 1.0.9 responsive header 40% done, looking for fix

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 $( document ).ready(function() {
-    $(".nav-min").click(function() {
+    $(".nav-min, .close--nav").click(function() {
         $(".nav").toggle();
-        console.log("toggle")
     })
     $("#search--button__nav").click(function() {
         $('.nav__search').toggleClass("active");
-    });
-});
+    })
+})
