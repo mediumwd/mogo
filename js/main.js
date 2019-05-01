a26b01de8ee2d4e6093f5a9b1a0c18505493d317
mediumwd <medium4uk@gmail.com>
2019-05-01 16:27:48 +0300

    beta 1.0.10 styling nav_min, needs few fixes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-$( document ).ready(function() {
-    $(".nav-min, .close--nav").click(function() {
+$(document).ready(function () {
+    $(".nav-min, .close--nav").click(function () {
         $(".nav").toggle();
     })
-    $("#search--button__nav").click(function() {
+    $("#search--button__nav").click(function () {
         $('.nav__search').toggleClass("active");
     })
+
+    $(window).resize(function () {
+        if ($(window).width() < 576) {
+            $(".nav__search").addClass("active");
+        } else {
+            $(".nav__search").removeClass("active");
+        }
+    });
 })
